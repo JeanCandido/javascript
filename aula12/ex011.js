565d52aea46d115ee1db6b1f4b0cef8a02995958
JeanCandido <candidojean098@gmail.com>
2024-03-13 19:31:45 -0300

    s

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 64
+console.log(`Voce tem ${idade} anos`)
+
+if (idade < 16) {
+    console.log(`Não Vota`)
+} else if (idade < 18 || idade > 65){
+    console.log('Voto Opicional')
+} else {
+    console.log('Voto Obrigatorio')
+}
