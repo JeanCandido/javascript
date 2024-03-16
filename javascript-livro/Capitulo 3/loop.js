043ad806583769c5bccad55cdffc90151d8a5ae2
JeanCandido <candidojean098@gmail.com>
2024-03-16 10:08:38 -0300

    Update loop.js

--- javascript-livro/Capitulo 3/loop.js
+++ javascript-livro/Capitulo 3/loop.js
@@ -4,16 +4,16 @@ for (let i = 1; i <= 5; i++) {
 }
 */
 
-/*
-let i = 1
-while (i <= 5) {
+let i = 10
+while (i >= 0) {
     console.log(i)
-    i++
+    i--
 }
-*/
 
-let i = -4
+/*
+let i = 5
 do {
     console.log(i)
     i++
 }while (i <= 5)
+*/
