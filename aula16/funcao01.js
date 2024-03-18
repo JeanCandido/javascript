ef67e19641ee04050c1699f70c6aafc696ef49a1
JeanCandido <candidojean098@gmail.com>
2024-03-18 15:34:18 -0300

    g

---
+++ aula16/funcao01.js
@@ -0,0 +1,11 @@
+function parimp(n){
+    if ( n == 0){
+        return 'Zero!'
+    }else if(n%2 == 0){
+        return 'Par!'
+    }else{
+        return 'Impar!'
+    }
+}
+
+console.log(parimp(609004198061))
