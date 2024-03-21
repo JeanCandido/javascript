b39a25740a6da10b59356d557fc46c9beffbd554
JeanCandido <candidojean098@gmail.com>
2024-03-21 15:32:58 -0300

    er

---
+++ aula16ex/ex18/script.js
@@ -0,0 +1,17 @@
+var nums = []
+
+var ntxt = document.getElementById('num')
+var res = document.querySelector('#res')
+var num = ntxt.value
+
+function add(){
+    let list = document.querySelector('#list')
+
+    res = ''
+
+    if ( num < 1 || num > 100 || isNaN(num)){
+        alert('Numero invalido' + num)
+    }else{
+        alert('vahdslhfj' + num)
+    }
+}
