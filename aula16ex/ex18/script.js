b620e249898715a20cb4f8dfaf37c50855710d25
JeanCandido <candidojean098@gmail.com>
2024-03-23 13:21:14 -0300

    ik

--- aula16ex/ex18/script.js
+++ aula16ex/ex18/script.js
@@ -1,17 +1,44 @@
 var nums = []
-
-var ntxt = document.getElementById('num')
-var res = document.querySelector('#res')
-var num = ntxt.value
+var list = document.querySelector('#list')
 
 function add(){
-    let list = document.querySelector('#list')
 
-    res = ''
+    var ntxt = document.getElementById('num')
+    var num = ntxt.value
 
     if ( num < 1 || num > 100 || isNaN(num)){
-        alert('Numero invalido' + num)
+        alert('Numero invalido')
+    }else if (nums.includes(num)){
+        alert('Voce não pode inserir um numero 2 vezes')
+    }else {
+        var noption = document.createElement('option')
+        nums.push(num)
+
+        noption.innerText = `numero ${num} adicionado`;
+        
+
+        list.add(noption)
+    }
+
+    ntxt.value = ''
+    ntxt.focus()
+}
+
+function finalizar(){
+    if (nums.length == 0 ){
+        alert('adicione numeros')
     }else{
-        alert('vahdslhfj' + num)
+        list.innerText = ''
+        nums.sort()
+        var res = document.querySelector('#res')
+        let maior = nums[nums.length - 1]
+        let menor = nums[0]
+
+
+        res.innerHTML += `<p>Ao todo temos ${nums.length} numeros cadastrados</p>`
+        res.innerHTML += `<p>maior valor inserido foi ${maior}</p>`
+        res.innerHTML += `<p>menor valor inserido foi ${menor}</p>`
+
+
     }
 }
